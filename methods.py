bf6a59899654e9a0dd4b1502c0ab0e68c2994ce2
c-laun <christian.laun@gmail.com>
2020-06-03 14:04:52 +0200

    started hexgrid

--- methods.py
+++ methods.py
@@ -56,7 +56,7 @@ class Automata:
             right = i+1
         if (right == self.cols):
             right = 0
-        if (self.cells[left].get_prev_state() <> self.cells[right].get_prev_state()):
+        if (self.cells[left].get_prev_state() != self.cells[right].get_prev_state()):
             self.cells[i].set_state(1)
         else:
             self.cells[i].set_state(0)
@@ -64,3 +64,54 @@ class Automata:
         center = int(self.cols / 2)
         
 # Game of Life
+
+class HexGrid:
+    """
+    Hexagonal grid that houses cells
+    """
+    def __init__(self, Celltype, height, width, cellkwds={}):
+        """
+        Celltype: The constructor function of the cells
+        cellkwds: dict that contains config for cell constructor
+        """
+
+        self.Celltype = Celltype
+        self.cells = []
+        self.height = height
+        self.width = width
+
+        for w in range(width):
+            self.cells += [[]]
+            for h in range(height):
+                self.cells[w] += [self.Celltype(**cellkwds)]
+
+    
+    def get_six_neighbours(self, x, y):
+        """
+        return the six adjacent cells to the cell at pos. x, y.
+        Assumes periodic boundary conditions.
+        """
+        left = (x-1)%self.width
+        right = (x+1)%self.width
+        up = (y+1)%self.heigth
+        down = (y-1)%self.height
+
+        return [self.cells[left][up], self.cells[x][up], 
+                self.cells[left][y], self.cells[right][y], 
+                self.cells[down][left], self.cells[down][y]]
+
+    def get_cell(self, x, y, w=0):
+        x = (x-w)%self.width
+        y = (y+w)%self.height
+
+        return self.cells[x][y]
+
+
+
+class NubotCell:
+    """
+    Cells of the Nubot model. Is supposed to live on the HexGrid.
+    """
+
+    def __init__(self):
+        raise NotImplementedError
